5e7b0d78c3e5ad7096b28829b6a64de040f8008b
Weng Wei <wuvist@gmail.com>
2024-02-03 23:12:58 +0800

    refine

--- data/download.py
+++ data/download.py
@@ -1,11 +1,11 @@
 import yfinance as yf
 
-tickers = open("ticker2024.txt", "r").readlines()
+tickers = open("../ticker2024.txt", "r").readlines()
 
 for ticker in tickers:
     ticker = ticker.strip()
     if ticker == "":
         continue
     data = yf.download(ticker + ".SI", start="2000-01-01", end="2024-01-31")
-    data.to_csv("data/" + ticker + ".csv",  float_format='%.6f')
+    data.to_csv(ticker + ".csv",  float_format='%.6f')
     print(ticker + " done")
