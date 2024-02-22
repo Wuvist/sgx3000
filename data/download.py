058a4dc63306c516cb7d3baed71ae856b5793c87
Weng Wei <wuvist@gmail.com>
2024-02-22 21:37:24 +0800

    Add Dividends,Stock Splits,Capital Gains data

--- data/download.py
+++ data/download.py
@@ -6,6 +6,7 @@ for ticker in tickers:
     ticker = ticker.strip()
     if ticker == "":
         continue
-    data = yf.download(ticker + ".SI", start="2000-01-01", end="2024-01-31")
+    t = yf.Ticker(ticker + ".SI")
+    data = t.history(start="2000-01-01", end="2024-01-31")
     data.to_csv(ticker + ".csv",  float_format='%.6f')
     print(ticker + " done")
