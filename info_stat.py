1a87aa6832a9b0cbcc29c599b4ec28d8829fe057
Wuvist <wuvist@gmail.com>
2024-03-11 14:40:48 +0800

    Add info_stat

---
+++ info_stat.py
@@ -0,0 +1,19 @@
+import json
+
+
+with open('data/info.json') as f:
+    info = json.load(f)
+
+sectors = dict()
+data = None
+for key, v in info.items():
+    try:
+        sector = v['sector']
+    except KeyError:
+        sector = "None"
+    if sector in sectors:
+        sectors[sector] = sectors[  sector] + 1
+    else:
+        sectors[sector] = 1
+
+print(json.dumps(sectors, ensure_ascii=False, indent=4))
