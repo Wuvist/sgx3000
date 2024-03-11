a47eedd884586c70df77805c06e6cbbbf1131622
Wuvist <wuvist@gmail.com>
2024-03-11 15:03:23 +0800

    Refactor and add industry

--- info_stat.py
+++ info_stat.py
@@ -1,18 +1,46 @@
 import json
+import sys
 
+cmd = sys.argv[1]
+
+info = None
 with open('data/info.json') as f:
     info = json.load(f)
 
-sectors = dict()
+def sector():
+    sectors = dict()
+    for key, v in info.items():
+        try:
+            sector = v['sector']
+        except KeyError:
+            sector = "None"
+        if sector in sectors:
+            sectors[sector] = sectors[sector] + 1
+        else:
+            sectors[sector] = 1
 
-for key, v in info.items():
-    try:
-        sector = v['sector']
-    except KeyError:
-        sector = "None"
-    if sector in sectors:
-        sectors[sector] = sectors[sector] + 1
-    else:
-        sectors[sector] = 1
+    print(json.dumps(sectors, ensure_ascii=False, indent=4))
 
-print(json.dumps(sectors, ensure_ascii=False, indent=4))
+def industry():
+    industries = dict()
+    for key, v in info.items():
+        try:
+            industry = v['industry']
+        except KeyError:
+            industry = "None"
+        if industry in industries:
+            industries[industry] = industries[industry] + 1
+        else:
+            industries[industry] = 1
+
+    print(json.dumps(industries, ensure_ascii=False, indent=4))
+
+
+# python info_stat.py industry | save industries.json
+# python info_stat.py sector | save sectors.json
+
+match cmd:
+    case "sector":
+        sector()
+    case "industry":
+        industry()
