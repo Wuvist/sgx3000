050c85f9fcdb39f18e908e0b759dc1ddcc032db7
Wuvist <wuvist@gmail.com>
2024-03-11 15:22:46 +0800

    Add industry_in_sector

--- info_stat.py
+++ info_stat.py
@@ -35,6 +35,33 @@ def industry():
 
     print(json.dumps(industries, ensure_ascii=False, indent=4))
 
+def count_item_by_key(data, key):
+    result = dict()
+    for item in data:
+        try:
+            val = item[key]
+        except KeyError:
+            val = "None"
+        if val in result:
+            result[val] = result[val] + 1
+        else:
+            result[val] = 1
+
+    return result
+
+def industry_in_sector():
+    sector = sys.argv[2]
+    industries = dict()
+    data = [v for key, v in info.items() if 'sector' in v and v['sector'] == sector]
+    if sector == "None":
+        data = [v for key, v in info.items() if not 'sector' in v]
+
+    print("Sector: " + sector)
+    industries =count_item_by_key(data, 'industry')
+    industries = dict(sorted(industries.items(), key=lambda x:x[1], reverse=True))
+
+    print(json.dumps(industries, ensure_ascii=False, indent=4))
+
 
 # python info_stat.py industry | save industries.json
 # python info_stat.py sector | save sectors.json
@@ -44,3 +71,5 @@ match cmd:
         sector()
     case "industry":
         industry()
+    case "industry_in_sector":
+        industry_in_sector()
