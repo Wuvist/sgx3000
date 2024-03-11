c04b0b1bc31857314e2052b29cba8d8480274598
Wuvist <wuvist@gmail.com>
2024-03-11 14:47:19 +0800

    clean up

--- info_stat.py
+++ info_stat.py
@@ -1,18 +1,17 @@
 import json
 
-
 with open('data/info.json') as f:
     info = json.load(f)
 
 sectors = dict()
-data = None
+
 for key, v in info.items():
     try:
         sector = v['sector']
     except KeyError:
         sector = "None"
     if sector in sectors:
-        sectors[sector] = sectors[  sector] + 1
+        sectors[sector] = sectors[sector] + 1
     else:
         sectors[sector] = 1
 
