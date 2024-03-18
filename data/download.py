2913ff283c17155165ffc637b9946651eb9b182f
Wuvist <wuvist@gmail.com>
2024-03-18 11:09:34 +0800

    Fix MXMU

--- data/download.py
+++ data/download.py
@@ -7,6 +7,8 @@ for ticker in tickers:
     if ticker == "":
         continue
     t = yf.Ticker(ticker + ".SI")
+    # data = t.history(start="2000-01-01", end="2024-01-31")
+    # data.to_csv(ticker + ".csv",  float_format='%.6f')
     data = t.history(start="2024-01-01", end="2024-03-18")
     data.to_csv("2024/" + ticker + ".csv",  float_format='%.6f')
     print(ticker + " done")
