77bcd5d3c5bccf7a4a3af5b772c89c68310befa8
Weng Wei <wuvist@gmail.com>
2024-03-29 13:37:54 +0800

    Add cal_dividend / cal_return

--- info_stat.py
+++ info_stat.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import sys
+from io import StringIO
 
 cmd = sys.argv[1]
 
@@ -92,7 +93,7 @@ def fetch():
         print(ticker + " no transaction history")
         return
 
-    dividends = df[df.Dividends >0]
+    dividends = df[df.Dividends > 0]
 
     if len(dividends) == 0:
         print("No dividen given")
@@ -122,7 +123,8 @@ def cal_rclose_one(ticker):
         if row.Dividends > 0:
             gap = row.Dividends / (i - last_dividend)
             for j in range(last_dividend + 1, i):
-                df.at[j, "RClose"] = df.iloc[j].RClose + gap*(j - last_dividend) + delta
+                df.at[j, "RClose"] = df.iloc[j].RClose + \
+                    gap*(j - last_dividend) + delta
             df.at[i, "RClose"] = row.RClose + row.Dividends + delta
 
             delta += row.Dividends
@@ -143,6 +145,129 @@ def cal_rclose():
         except FileNotFoundError:
             print(ticker + " not found")
 
+
+def load_dividend(ticker):
+    fname = "data/" + ticker + ".csv"
+    df = pd.read_csv(fname)
+    df = df[df['Dividends'] > 0]
+    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    df['Day'] = df['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    return df
+
+
+def load_price(ticker):
+    fname = "data_latest/" + ticker + ".csv"
+    df = pd.read_csv(fname)
+    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    df["Day"] = df['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    return df
+
+
+class Stock:
+    def __init__(self, price, dividend):
+        self.price = price
+        self.dividend = dividend
+
+
+def load(ticker):
+    return Stock(load_price(ticker), load_dividend(ticker))
+
+
+def list_dividend(backday, stock):
+    df = stock.dividend[stock.dividend["Day"] >= backday]
+    print("== csv")
+    print("")
+
+    result = ""
+
+    result += "Day,Close,Original Close,Dividends,Adjustment Factor\n"
+
+    # Initialize the total adjustment factor
+    total_adjustment_factor = 1
+
+    # Iterate through the DataFrame in reverse order
+    for i in range(len(df) - 1, -1, -1):
+        row = df.iloc[i]
+        oclose = row['Close'] / total_adjustment_factor
+        adjustment_factor = oclose / (oclose + row['Dividends'])
+        total_adjustment_factor *= adjustment_factor
+
+        # Print the calculated values
+        result += f"{row['Day']},{row['Close']},{oclose},{row['Dividends']},{adjustment_factor}\n"
+
+    print(result)
+    # Print the total adjustment factor
+    print("\ntotal_adjustment_factor:", total_adjustment_factor)
+
+    print("")
+    df = pd.read_csv(StringIO(result))
+    return df
+
+
+def cal_dividend():
+    day = sys.argv[2].upper()
+    ticker = sys.argv[3].upper()
+
+    stock = load(ticker)
+
+    print(list_dividend(day, stock))
+
+
+class StockReturn:
+    def __init__(self, ByAdjClose, ByActual):
+        self.ByAdjClose = ByAdjClose
+        self.ByActual = ByActual
+
+
+NaN = float('nan')
+
+
+def get_return_diff(r):
+    return r.ByAdjClose / r.ByActual - 1
+
+
+def get_return(buy_day, sell_day, stock):
+    # Finding the index of the buy_day and sell_day
+    buy_index = stock.price[stock.price['Day'] <= buy_day].index.max()
+    sell_index = stock.price[stock.price['Day'] <= sell_day].index.max()
+
+    if pd.isna(buy_index) or pd.isna(sell_index):
+        return StockReturn(NaN, NaN)
+
+    buy = stock.price.loc[buy_index]
+    sell = stock.price.loc[sell_index]
+    print("# Return by Adj Close")
+    print("buy: {:.6f} sell: {:.6f} gain: {:.6f}\n".format(buy["Adj Close"],
+                                                           sell["Adj Close"],
+                                                           sell["Adj Close"] - buy["Adj Close"]))
+
+    ReturnByAdjClose = sell["Adj Close"] / buy["Adj Close"] - 1
+
+    dividends = stock.dividend[(stock.dividend.Day > buy_day) & (
+        stock.dividend.Day < sell_day)].Dividends.sum()
+
+    print("Dividend: {:.6f}\n".format(dividends))
+    ByActual = (sell["Close"] + dividends) / buy["Close"] - 1
+
+    print("# Actual Return by adding back dividend")
+    print("buy: {:.6f} sell: {:.6f} gain: {:.6f}\n".format(buy["Close"],
+                                                           sell["Close"] +
+                                                           dividends,
+                                                           sell["Close"] + dividends - buy["Close"]))
+    r = StockReturn(ReturnByAdjClose, ByActual)
+    print("# Return difference")
+    print(get_return_diff(r))
+    return r
+
+
+def cal_return():
+    buy_day = sys.argv[2].upper()
+    sell_day = sys.argv[3].upper()
+    ticker = sys.argv[4].upper()
+
+    stock = load(ticker)
+
+    get_return(buy_day, sell_day, stock)
 
 # python info_stat.py industry | save industries.json
 # python info_stat.py sector | save sectors.json
@@ -168,5 +293,9 @@ match cmd:
         find_sector()
     case "find_industry":
         find_industry()
+    case "cal_return":
+        cal_return()
+    case "cal_dividend":
+        cal_dividend()
     case "cal_rclose":
         cal_rclose()
